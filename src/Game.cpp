8496dd30d1c9a7665e77c7bbd1c62891718cf232
DavideCrisafulli <davidecrisafull2015@gmail.com>
2019-02-09 21:39:09 +0100

    added a new method to close the window

--- src/Game.cpp
+++ src/Game.cpp
@@ -7,11 +7,10 @@ void Game::initGame(){
     al_init();
     al_init_image_addon();
     al_install_keyboard();
-
 }
 
 void Game::initEvent(){
-    
+
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
     ALLEGRO_EVENT_QUEUE *event_queque = al_create_event_queue();
     al_register_event_source(event_queque,al_get_keyboard_event_source());
